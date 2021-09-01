10b3e5ef89efb41a2ff93f50e1af74c063541e0f
Jake Metherell <jake@fluffylogic.com>
2021-09-01 11:52:00 +0100

    Use ApplicationType instead of Application tag to find nfs

--- ansible/roles/lifecycle-scripts/files/boot-strap.sh
+++ ansible/roles/lifecycle-scripts/files/boot-strap.sh
@@ -2,7 +2,7 @@
 
 ### Script to start Docker containers using Docker Compose.
 ### Requires values to be supplied as tags on the EC2 instance where it runs:
-###   Application  (the name of the application type - e.g. cics or chips etc)
+###   ApplicationType  (the name of the application type - e.g. cics or chips etc)
 ###   app-instance-name  (the name of the application instance type - e.g. cics1 or chips-ef-batch1 etc)
 ###   config-base-path (the S3 path to where the config is stored - e.g. s3://shared-services.eu-west-2.configs.ch.gov.uk/cic-configs/development etc)
 ### The tags are defined in terraform code and set as values on the launch configuration that is used by the Auto Scaling Group to create EC2 instances.
@@ -18,8 +18,8 @@ EC2_REGION=$( ec2-metadata -z | awk '{print $2}' | sed 's/[a-z]$//' )
 EC2_INSTANCE_ID=$( ec2-metadata -i |  awk -F'[: ]' '{print $3}' )
 echo "EC2_INSTANCE_ID=${EC2_INSTANCE_ID}"
 
-# Get Application TAG from EC2 instance
-APP_NAME=$( aws ec2 describe-tags --filters "Name=resource-id,Values=${EC2_INSTANCE_ID}" --region ${EC2_REGION} --output text|grep Application|  awk '{print $5}' | tr '[:upper:]' '[:lower:]' )
+# Get ApplicationType TAG from EC2 instance
+APP_NAME=$( aws ec2 describe-tags --filters "Name=resource-id,Values=${EC2_INSTANCE_ID}" --region ${EC2_REGION} --output text|grep ApplicationType|  awk '{print $5}' | tr '[:upper:]' '[:lower:]' )
 echo "APP_NAME=${APP_NAME}"
 
 INSTANCE_DIR=${NFS_MOUNT_LOCATION}/${APP_NAME}
